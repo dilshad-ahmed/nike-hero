438a6f6c6fd162df53cd9b087b3b76c9dec4ea68
dilshad ahmed <dilshad7427@gmail.com>
2024-07-14 20:35:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <main className="overflow-x-hidden">
+      <Navbar />
+      <Hero />
+    </main>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#c11912",
+      },
+
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        anton: ["Anton SC", "sans-serif"],
+        barlow: ["Barlow Semi Condensed", "sans-serif"],
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
